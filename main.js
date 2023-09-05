397bc1abb13c5a3af76b621f9789856ab7417b3b
BlakeR06 <113364139+BlakeR06@users.noreply.github.com>
2023-09-05 13:22:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function returnToMenu(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src1/index.html';
+}
+
+function beer(){
+    window.location.href = 'beer.html';
+}
+
+function wine(){
+    window.location.href = 'wine.html';
+}
+
+function mimosas(){
+    window.location.href = 'Mimosas.html';
+}
+
+function backToDrinks(){
+    window.location.href = 'drinks.html';
+}
